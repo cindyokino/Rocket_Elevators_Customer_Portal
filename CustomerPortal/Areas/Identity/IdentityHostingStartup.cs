029ed220b9eaebb36c0e6ac6a0060a5564fe4969
Cindy Okino <cindy.okino@gmail.com>
2020-12-01 13:31:10 -0500

    Add project files.

---
+++ CustomerPortal/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CustomerPortal.Areas.Identity.Data;
+using CustomerPortal.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CustomerPortal.Areas.Identity.IdentityHostingStartup))]
+namespace CustomerPortal.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<DbCustomerPortalContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DbCustomerPortalContextConnection")));
+
+                services.AddDefaultIdentity<CustomerPortalUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<DbCustomerPortalContext>();
+            });
+        }
+    }
+}
