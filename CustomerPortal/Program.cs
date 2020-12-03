9d7d1f2208201d50fb69f68e6202bab5d13b809f
Cindy Okino <cindy.okino@gmail.com>
2020-12-03 00:15:14 -0500

    Small modifications

--- CustomerPortal/Program.cs
+++ CustomerPortal/Program.cs
@@ -6,11 +6,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
 
 namespace CustomerPortal
 {
     public class Program
     {
+         
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
